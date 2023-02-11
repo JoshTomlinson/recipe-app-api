b504c73fb795cabae62b5a40845266b1941d1de4
Joshua Tomlinson <joshuatomlinson@Joshuas-MacBook-Air.local>
2023-02-11 18:59:14 +0000

    user_tests_added

---
+++ app/user/urls.py
@@ -0,0 +1,12 @@
+"""
+URL mappings for the user API
+"""
+from django.urls import path
+
+from user import views
+
+ap_name = 'user'
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+]
